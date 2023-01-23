5d3130a62a3989c3fe0c31f68870cfc089868084
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2023-01-23 15:27:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+// detecting button press
+var noOfButtons = document.querySelectorAll(".drum").length;
+for (let i = 0; i < noOfButtons; i++) {
+   
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+         makeSound(this.innerHTML);
+         buttonAnimation(this.innerHTML);
+     })
+    
+} 
+// detecting keyboard press
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+            
+            break;
+
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play();
+                
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                
+            break;
+    
+        case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+                
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+            
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+            
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+            
+            break;
+            default:
+                console.log(this.innerHTML)
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    document.querySelector("."+currentKey).classList.add("pressed");
+    setTimeout(function () {
+    document.querySelector("."+currentKey).classList.remove("pressed");
+    }, 200);
+}
